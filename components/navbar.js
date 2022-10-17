bdd2768fbd6284e5852a7faded8fff544e3c8fb2
vigneshkanna <vkrishitaran@gmail.com>
2022-10-17 21:18:41 +0530

    desktop navbar added

---
+++ components/navbar.js
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <div className="nav_container">
+      <nav>
+        <Image src="/nav_logo.png" alt="logo" width={240} height={36} />
+        <div className="right_contents">
+          <div className="nav_links">
+            <Link href="/">Components</Link>
+          </div>
+          <div className="nav_links">
+            <Link href="/">Documentation</Link>
+          </div>
+          <div className="nav_links">
+            <Link href="/">About Us</Link>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/components/navbar_style.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
