cbfe31b7a0cdcc78ea0682df3d983ccfaf138937
zainbinfurqan <zain.ahmed199524@gmail.com>
2022-10-18 23:00:23 +0500

    worked on delete button creating

--- tailwind_components/buttons/collection.js
+++ tailwind_components/buttons/collection.js
@@ -1,9 +1,11 @@
 import test from "./test_file";
 import test1 from "./test1_file";
+import delete_button from "./delete_button";
 
 const buttonCollections = [
-  {componentName: "button1", component: test},
-  {componentName: "button2", component: test1},
+  { componentName: "button1", component: test },
+  { componentName: "button2", component: test1 },
+  { componentName: "delete button", component: delete_button },
 ];
 
 export default buttonCollections;
---
+++ tailwind_components/buttons/delete_button.jsx
@@ -0,0 +1,34 @@
+import { stripIndent } from "react-codepen-prefill-embed";
+
+const deleteButton = stripIndent`<div class="bg-gray-50">
+<div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
+  <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+    <span class="block">Ready to dive in?</span>
+    <span class="block text-indigo-600">Start your free trial today.</span>
+  </h2>
+  <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+    <div class="inline-flex rounded-md shadow">
+      
+      <a
+        href="#"
+        class="inline-flex items-center justify-center rounded-sm border border-transparent bg-red-200 px-5 py-3 text-base font-medium text-red-500 hover:bg-indigo-700">
+        <span class="mx-2">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+<path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+</svg>
+</span>
+       Delete
+      </a>
+    </div>
+    <div class="ml-3 inline-flex rounded-md shadow">
+      <a
+        href="#"
+        class="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600 hover:bg-indigo-50">
+        Learn more
+      </a>
+    </div>
+  </div>
+</div>
+</div>`;
+
+export default deleteButton;
