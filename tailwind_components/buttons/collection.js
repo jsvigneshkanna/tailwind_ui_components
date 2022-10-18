bd80838150803715f9217231eb912032527b2000
Sarosh Farhan <saroshfarhan6495@gmail.com>
2022-10-18 23:17:59 +0530

    added a card with button using tailwind css

--- tailwind_components/buttons/collection.js
+++ tailwind_components/buttons/collection.js
@@ -1,9 +1,11 @@
 import test from "./test_file";
 import test1 from "./test1_file";
+import cardWithButton from "./card_with_button_button1";
 
 const buttonCollections = [
   {componentName: "button1", component: test},
   {componentName: "button2", component: test1},
+  {componentName: "Card with button", component: cardWithButton},
 ];
 
 export default buttonCollections;
