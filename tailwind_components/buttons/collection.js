2184a26b737c029c60c45857a60b547dbfa51874
Amodu Kehinde <amodukehindee@gmail.com>
2022-10-18 19:37:35 +0100

    created see mre button

--- tailwind_components/buttons/collection.js
+++ tailwind_components/buttons/collection.js
@@ -1,9 +1,11 @@
 import test from "./test_file";
 import test1 from "./test1_file";
+import see_more1 from './see_more1'
 
 const buttonCollections = [
   {componentName: "button1", component: test},
   {componentName: "button2", component: test1},
+  {componentName: "button3", component: see_more1},
 ];
 
 export default buttonCollections;
