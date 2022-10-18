8a33d166fdd5786f4beb4486752e70a71d3bdac4
Amodu Kehinde <amodukehindee@gmail.com>
2022-10-18 21:04:09 +0100

    create download now

--- tailwind_components/buttons/collection.js
+++ tailwind_components/buttons/collection.js
@@ -1,9 +1,11 @@
 import test from "./test_file";
 import test1 from "./test1_file";
+import downloadnow from './download_now1.jsx'
 
 const buttonCollections = [
   {componentName: "button1", component: test},
   {componentName: "button2", component: test1},
+  {componentName: "Download now button", component: downloadnow},
 ];
 
 export default buttonCollections;
