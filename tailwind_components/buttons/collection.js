be2f533a5e222b66229a884662fa075e554460be
itsmenikhill <nklsharma2152@gmail.com>
2022-10-19 17:08:41 +0530

    rectangle dropdown button added

--- tailwind_components/buttons/collection.js
+++ tailwind_components/buttons/collection.js
@@ -1,9 +1,11 @@
 import test from "./test_file";
 import test1 from "./test1_file";
+import dropdownButton from "./rectangle_dropdown_button";
 
 const buttonCollections = [
   {componentName: "button1", component: test},
   {componentName: "button2", component: test1},
+  {componentName: "button3", component: dropdownButton},
 ];
 
 export default buttonCollections;
