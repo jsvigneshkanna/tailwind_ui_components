1b165b08183e501b0be30beba720ce1d6866925f
vigneshkanna <vkrishitaran@gmail.com>
2022-10-19 18:40:21 +0530

    no link enable in component page

--- pages/components/index.js
+++ pages/components/index.js
@@ -1,4 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
+/* eslint-disable @next/next/no-html-link-for-pages */
+
 import Head from "next/head";
 import Navbar from "../../components/navbar";
 import Image from "next/image";
