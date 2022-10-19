78949031748c401adea71da69fc094b61f88a382
Chibuike-19 <emmanuelobiechina8@gmail.com>
2022-10-19 11:03:08 +0100

    responsive navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,27 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import {MdMenu} from 'react-icons/md'
+import {AiOutlineClose} from 'react-icons/ai'
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const showMenu = () => {
+    setIsMenuOpen(prev => !prev)
+  }
+  const displayMenu = () => {
+    if(isMenuOpen){
+      return 'flex sm:hidden flex-col justify-center items-center '
+    }else{
+      return 'hidden'
+    }
+  }
   return (
     <div className="nav_container">
       <nav>
         <Image src="/nav_logo.png" alt="logo" width={240} height={36} />
-        <div className="right_contents">
+        <div className='right_contents'>
           <div className="nav_links">
             <Link href="/">Components</Link>
           </div>
@@ -17,7 +32,21 @@ const Navbar = () => {
             <Link href="/">About Us</Link>
           </div>
         </div>
+        <div className='flex sm:hidden' onClick={showMenu}>
+          <p>{isMenuOpen ? <AiOutlineClose/> : <MdMenu/>}</p>
+        </div>
       </nav>
+        <div className={displayMenu()}>
+          <div className="nav_links">
+            <Link href="/">Components</Link>
+          </div>
+          <div className="nav_links">
+            <Link href="/">Documentation</Link>
+          </div>
+          <div className="nav_links">
+            <Link href="/">About Us</Link>
+          </div>
+        </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,16 @@ import Navbar from "../components/navbar";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Tailwind Bootstrap</title>
         <meta name="description" content="TailwindCSS ui component bootstrap" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className=" bg-slate-100 min-h-screen">
+      <div className=" bg-slate-100 w-full min-h-screen">
         <Navbar />
       </div>
-    </div>
+    </>
   );
 }
