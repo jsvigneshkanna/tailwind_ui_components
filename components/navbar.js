ae947252af9a435a4a42d5c40fd00f6842a90779
Chibuike-19 <emmanuelobiechina8@gmail.com>
2022-10-19 11:55:01 +0100

    resolve conflicts

--- components/navbar.js
+++ components/navbar.js
@@ -3,9 +3,11 @@ import Link from 'next/link';
 import {MdMenu} from 'react-icons/md'
 import {AiOutlineClose} from 'react-icons/ai'
 import { useState } from 'react';
+import {useRouter} from "next/router";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const router = useRouter();
 
   const showMenu = () => {
     setIsMenuOpen(prev => !prev)
@@ -20,7 +22,7 @@ const Navbar = () => {
   return (
     <div className="nav_container">
       <nav>
-        <Image src="/nav_logo.png" alt="logo" width={240} height={36} />
+        <Image src="/nav_logo.png" alt="logo" width={240} height={36} onClick={() => router.push('/')}/>
         <div className='right_contents'>
           <div className="nav_links">
             <Link href="/">Components</Link>
