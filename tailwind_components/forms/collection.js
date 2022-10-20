3b30c8699456395786662634e54d6f5199286472
Anuran Das <104062485+Anuran12@users.noreply.github.com>
2022-10-20 12:06:02 +0530

    added input form component

--- tailwind_components/forms/collection.js
+++ tailwind_components/forms/collection.js
@@ -1,11 +1,13 @@
 import sliderInputForm from "./slider_input_form";
 import Checkbox from "./checkbox";
 import searchInput from "./search_input_form";
+import inputForm from "./simple_input_form";
 
 const formCollections = [
   {componentName: "Range Slider", component: sliderInputForm},
   {componentName: "Check Box", component: Checkbox},
   {componentName: "Search Input", component: searchInput},
+  {componentName: "Input Form", component: inputForm},
 ];
 
 export default formCollections;
