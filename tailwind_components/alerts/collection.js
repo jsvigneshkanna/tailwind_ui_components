23a0090cfd12573bd0b195d6e5854e57a8ebec41
GAURCODER <lcs2021008@iiitl.ac.in>
2022-10-20 19:37:06 +0530

    #88 Added tick alert

--- tailwind_components/alerts/collection.js
+++ tailwind_components/alerts/collection.js
@@ -1,9 +1,11 @@
 import test from "./test_file";
 import infoAlert from "./info_alert";
+import tickAlert from "./tick_alert";
 
 const alertCollections = [
   {componentName: "test alert", component: test},
   {componentName: "Info Alert", component: infoAlert},
+  {componentName: "Tick Alert", component: tickAlert},
 ];
 
 export default alertCollections;
