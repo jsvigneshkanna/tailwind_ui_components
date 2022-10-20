66a60ccbbc849a2368b651aaafd02e7df943f805
J S Vignesh Kanna <42484705+jsvigneshkanna@users.noreply.github.com>
2022-10-20 19:53:51 +0530

    Update collection.js

--- tailwind_components/alerts/collection.js
+++ tailwind_components/alerts/collection.js
@@ -2,7 +2,6 @@ import infoAlert from "./info_alert";
 import tickAlert from "./tick_alert";
 
 const alertCollections = [
-  {componentName: "test alert", component: test},
   {componentName: "Info Alert", component: infoAlert},
   {componentName: "Tick Alert", component: tickAlert},
 ]
