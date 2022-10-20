93df3b4582769267001aff5cdd61b6187b1534e4
Amodu Kehinde <amodukehindee@gmail.com>
2022-10-20 08:28:54 +0100

    create amazon button

--- tailwind_components/store_buttons/collection.js
+++ tailwind_components/store_buttons/collection.js
@@ -1,7 +1,9 @@
 import test from "./test_file";
+import amazonStoreButton from "./amazon_store_button";
 
 const storeButtonCollections = [
   {componentName: "test store button", component: test},
+  {componentName: "Amazon Store Button", component: amazonStoreButton},
 ];
 
 export default storeButtonCollections;
