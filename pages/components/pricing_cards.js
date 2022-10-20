8cc30212d1993805abbb81a6a06968ac6e00761a
vigneshkanna <vkrishitaran@gmail.com>
2022-10-20 20:52:59 +0530

    pricing card paeg added

---
+++ pages/components/pricing_cards.js
@@ -0,0 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
+import Head from "next/head";
+import Navbar from "../../components/navbar";
+import Codepen from "../../components/codepen";
+// import test from "../tailwind_components/buttons/test_file.jsx";
+import pricingCardsCollections from "../../tailwind_components/pricing_cards/collection";
+
+const PricingCards = () => {
+  return (
+    <div>
+      <Head>
+        <title>Tailwind UI Pricing Cards</title>
+        <meta name="description" content="TailwindCSS ui component bootstrap" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+
+      <div className="codepen_page font-poppins">
+        <p className="codepen_brief">
+          Have cool product (Not having? believe you have) but not sure how can
+          you sell/ market those pricings. Our developers have designed the best
+          pricing templates, Just tweak the text and you are good to go 💰
+        </p>
+        {pricingCardsCollections.map((pricingCard, index) => {
+          return (
+            <div key={index} className="codepen_container">
+              <h3 className="codepen_name">
+                {index + 1}. {pricingCard.componentName}
+              </h3>
+              <Codepen htmlText={pricingCard.component} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default PricingCards;
