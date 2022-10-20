1b761f307787bc78f16878f4a077edb32f344991
Kabiir <kabiirk@gmail.com>
2022-10-21 02:51:00 +0530

    Initialize boilerplate files and updated collection.js

--- tailwind_components/pricing_cards/collection.js
+++ tailwind_components/pricing_cards/collection.js
@@ -1,7 +1,9 @@
 import samplePricingCard from "./sample_pricing_card";
+import smallPricingCard from "./small_pricing_card";
 
 const pricingCardsCollections = [
   {componentName: "Sample Pricing Card", component: samplePricingCard},
+  {componentName: "Small Pricing Card", component: smallPricingCard},
 ];
 
 export default pricingCardsCollections;
