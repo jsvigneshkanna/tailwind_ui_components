178089f9713c7943f7414ecf9a6fed04764701b1
Amodu Kehinde <amodukehindee@gmail.com>
2022-10-20 08:42:30 +0100

    create amazon button

--- tailwind_components/store_buttons/amazon_store_button.jsx
+++ tailwind_components/store_buttons/amazon_store_button.jsx
@@ -19,8 +19,8 @@ import {stripIndent} from "react-codepen-prefill-embed";
   <i class="fa-brands fa-amazon text-4xl"></i>
   </span>
   <a href='#'>
-  <p class='text-[13px] font-bold'>Download on the</p>
-  <p class='text-xl'>Amazon.com</p>
+  <p class='text-xs font-bold'>Download on the</p>
+  <p class='text-lg'>Amazon.com</p>
   </a>
 </button>
   </div>
@@ -31,8 +31,8 @@ import {stripIndent} from "react-codepen-prefill-embed";
   <i class="fa-brands text-white fa-amazon text-4xl"></i>
   </span>
   <a class='text-white block' href='#'>
-  <p class='text-[13px] font-bold'>Download on the</p>
-  <p class='text-xl'>Amazon.com</p>
+  <p class='text-xs font-bold'>Download on the</p>
+  <p class='text-lg'>Amazon.com</p>
   </a>
 </button>
   </div>
