43e15b3c4d7d2ffafd737d9f07e47bea445b7267
J S Vignesh Kanna <42484705+jsvigneshkanna@users.noreply.github.com>
2022-10-21 15:07:21 +0530

    Update collection.js

--- tailwind_components/pricing_cards/collection.js
+++ tailwind_components/pricing_cards/collection.js
@@ -1,9 +1,9 @@
 import samplePricingCard from "./sample_pricing_card";
-import vertical_pricing from "./vertical_pricing";
+import verticalPricing from "./vertical_pricing";
 
 const pricingCardsCollections = [
   {componentName: "Sample Pricing Card", component: samplePricingCard},
-  {componentName: "Vertical Pricing Card", coomponent: vertical_pricing},
+  {componentName: "Vertical Pricing Card", coomponent: verticalPricing},
 ];
 
 export default pricingCardsCollections;
