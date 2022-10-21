4a17738060e4124e09c88a6fc60ff39fcddad2ae
streetcodec <streetcodec@gmail.com>
2022-10-21 14:55:21 +0530

    Added vertical pricing card component

--- tailwind_components/pricing_cards/collection.js
+++ tailwind_components/pricing_cards/collection.js
@@ -1,7 +1,9 @@
 import samplePricingCard from "./sample_pricing_card";
+import vertical_pricing from "./vertical_pricing";
 
 const pricingCardsCollections = [
   {componentName: "Sample Pricing Card", component: samplePricingCard},
+  {componentName: "Vertical Pricing Card", coomponent: vertical_pricing},
 ];
 
 export default pricingCardsCollections;
