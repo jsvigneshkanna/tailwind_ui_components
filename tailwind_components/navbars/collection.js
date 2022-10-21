4d1f2a01a91dd4998c7e488bd590f2ae96f196e1
streetcodec <streetcodec@gmail.com>
2022-10-22 00:20:12 +0530

    Added Animated navbar Component

--- tailwind_components/navbars/collection.js
+++ tailwind_components/navbars/collection.js
@@ -1,7 +1,9 @@
 import test from "./test_file";
+import animated_navbar from "./animated_navbar";
 
 const navbarCollections = [
   {componentName: "Sample Dev codepen", component: test},
+  {componentName: "Animated Navbar", component: animated_navbar},
 ];
 
 export default navbarCollections;
