308d0ceaa1b65faa578dd5ee0a6b779d5cb01ab1
Anuran Das <104062485+Anuran12@users.noreply.github.com>
2022-10-21 11:13:55 +0530

    added product card component

--- tailwind_components/feature_cards/collection.js
+++ tailwind_components/feature_cards/collection.js
@@ -1,9 +1,11 @@
 import cardWithButton from "./card_with_button_button1";
 import brandCard from "./brand_card";
+import productCard from "./product_card1";
 
 const featureCardColletion = [
   {componentName: "Card With Button", component: cardWithButton},
   {componentName: "Brand Card", component: brandCard},
+  {componentName: "Product Card", component: productCard},
 ];
 
 export default featureCardColletion;
