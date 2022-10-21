a2bd9d7751b00a6de45fe12f9e738800d6738cac
streetcodec <streetcodec@gmail.com>
2022-10-22 01:07:03 +0530

    Added Mobile Nav Icons

--- tailwind_components/navbars/collection.js
+++ tailwind_components/navbars/collection.js
@@ -1,9 +1,11 @@
 import test from "./test_file";
 import animated_navbar from "./animated_navbar";
+import mobile_nav_icons from "./mobile_nav_icons";
 
 const navbarCollections = [
   {componentName: "Sample Dev codepen", component: test},
   {componentName: "Animated Navbar", component: animated_navbar},
+  {componentName: "Mobile NavBar Icons", component: mobile_nav_icons},
 ];
 
 export default navbarCollections;
