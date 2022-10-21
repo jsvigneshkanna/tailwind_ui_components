52c87991319f7e0ab1c19f4ca68aa5012392e4ce
Deepak Suthar <92250394+deepaksuthar40128@users.noreply.github.com>
2022-10-21 07:30:10 +0530

    Update collection.js

--- tailwind_components/alerts/collection.js
+++ tailwind_components/alerts/collection.js
@@ -1,5 +1,9 @@
 import test from "./test_file";
+import alarmAlert from "./alarm_alert";
 
-const alertCollections = [{componentName: "test alert", component: test}];
+const alertCollections = [
+  {componentName: "test alert", component: test},
+  {componentName: "Alarm Alert", component: alarmAlert},
+];
 
 export default alertCollections;
