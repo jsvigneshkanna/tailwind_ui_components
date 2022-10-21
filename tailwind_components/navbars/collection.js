e4edb4abe660600c9c2d7c3e50d2141e608837aa
streetcodec <streetcodec@gmail.com>
2022-10-22 04:07:47 +0530

    Added Mobile Nav Dropdown Component

--- tailwind_components/navbars/collection.js
+++ tailwind_components/navbars/collection.js
@@ -1,11 +1,12 @@
 import test from "./test_file";
 import animated_navbar from "./animated_navbar";
 import mobile_nav_icons from "./mobile_nav_icons";
-
+import mobile_nav_dropdown from "./mobile_nav_dropdown";
 const navbarCollections = [
   {componentName: "Sample Dev codepen", component: test},
   {componentName: "Animated Navbar", component: animated_navbar},
   {componentName: "Mobile NavBar Icons", component: mobile_nav_icons},
+  {componentName: "Mobile Nav DropDown", component: mobile_nav_dropdown}
 ];
 
 export default navbarCollections;
