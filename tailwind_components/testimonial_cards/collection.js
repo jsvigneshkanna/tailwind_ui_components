8c67683285bc1139de56d02e4f7869f711286eb7
Bhavya Pratap Singh Tomar <bhavyapratapsinghtomar@gmail.com>
2022-10-22 06:03:41 +0530

    photo_star_testimonial.jsx added to the testimonial cards collection and collection.js is updated by adding the respective component

--- tailwind_components/testimonial_cards/collection.js
+++ tailwind_components/testimonial_cards/collection.js
@@ -1,9 +1,11 @@
 import test from "./test_file";
 import testimonialCard from "./vertical_testimonial";
+import photoStarTestimonial from "./photo_star_testimonial"
 
 const testimonialCardsCollections = [
   {componentName: "Sample Testimonial", component: test},
   {componentName: "Vertical testimonial card", component: testimonialCard},
+  {componentName: "Photo Star Testimonial", component: photoStarTestimonial}
 ];
 
 export default testimonialCardsCollections;
