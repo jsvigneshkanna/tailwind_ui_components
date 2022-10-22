e56b89f1f9b428688271c6d8a0ef949f6098aa1b
J S Vignesh Kanna <42484705+jsvigneshkanna@users.noreply.github.com>
2022-10-22 11:23:35 +0530

    added border-radius to img

--- tailwind_components/testimonial_cards/social_link_testimonial.jsx
+++ tailwind_components/testimonial_cards/social_link_testimonial.jsx
@@ -2,13 +2,13 @@ import {stripIndent} from "react-codepen-prefill-embed";
 
 const socialLinkTestimonial = stripIndent`
 
-<div class="flex h-screen w-screen items-center justify-center bg-slate-600">
-  <div class="h-96 w-60 max-w-sm rounded-xl border border-black bg-gray-300 shadow-md">
+<div class="flex h-screen w-screen items-center justify-center bg-slate-400">
+  <div class="h-96 w-60 max-w-sm rounded-xl border border-black bg-gray-300 shadow-lg">
     <a href="#"> </a>
     <div class="p-5 text-center">
       <a href="#"> </a>
       <div class="h-60 w-54  max-w-full h-auto rounded-lg">
-        <img src=https://images.squarespace-cdn.com/content/v1/53b599ebe4b08a2784696956/1438211096322-NBOR72MPHBEED1BSD36Q/image-asset.jpeg"  height="180" width="180" class = "object-cover h-60 w-60" >
+        <img src=https://images.squarespace-cdn.com/content/v1/53b599ebe4b08a2784696956/1438211096322-NBOR72MPHBEED1BSD36Q/image-asset.jpeg"  height="180" width="180" class = "object-cover h-60 w-60 rounded-lg" >
       </div>
       <ul class = "flex-col space-y-2" ><p><b> Username</b></p>
       <ul class = "flex-col space-y-2" ><p>Profession</p></ul>
