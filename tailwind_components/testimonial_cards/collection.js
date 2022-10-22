f05882242871d34ced986dc2f75bf483d653fcc7
J S Vignesh Kanna <42484705+jsvigneshkanna@users.noreply.github.com>
2022-10-22 11:22:40 +0530

    added comma

--- tailwind_components/testimonial_cards/collection.js
+++ tailwind_components/testimonial_cards/collection.js
@@ -6,7 +6,7 @@ import socialLinkTestimonial from "./social_link_testimonial"
 const testimonialCardsCollections = [
   {componentName: "Sample Testimonial", component: test},
   {componentName: "Vertical testimonial card", component: testimonialCard},
-  {componentName: "Photo Star Testimonial", component: photoStarTestimonial}
+  {componentName: "Photo Star Testimonial", component: photoStarTestimonial},
   {componentName: "Social Link Testimonial", component: socialLinkTestimonial}
 ];
 
