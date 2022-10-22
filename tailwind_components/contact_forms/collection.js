04930f811dd2d9147a695cbd80265fe1c97535de
Anuran Das <104062485+Anuran12@users.noreply.github.com>
2022-10-22 17:26:04 +0530

    added Vertical contact form component

--- tailwind_components/contact_forms/collection.js
+++ tailwind_components/contact_forms/collection.js
@@ -1,7 +1,9 @@
 import Checkbox from "./checkbox";
+import VerticalContactForm from "./checkbox";
 
 const contactFormsCollection = [
   {componentName: "Sample Contact form", component: Checkbox},
+  {componentName: "Vertical contact form", component: VerticalContactForm},
 ];
 
 export default contactFormsCollection;
