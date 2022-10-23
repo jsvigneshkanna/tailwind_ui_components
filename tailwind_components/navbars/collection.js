a789500d758021295833bcde308407ddea9805fd
Uttam Pandit <uttampndt28@gmail.com>
2022-10-23 19:46:22 +0530

    Added standard Logo navbar

--- tailwind_components/navbars/collection.js
+++ tailwind_components/navbars/collection.js
@@ -3,13 +3,14 @@ import mobile_nav_icons from "./mobile_nav_icons";
 import mobile_nav_dropdown from "./mobile_nav_dropdown";
 import standard_mobile_navbar from "./standard_mobile_navbar";
 import searchNav from "./search_navbar";
-
+import standardLogoNav from "./standard_logo_navbar";
 const navbarCollections = [
   {componentName: "Animated Navbar", component: animated_navbar},
   {componentName: "Mobile NavBar Icons", component: mobile_nav_icons},
   {componentName: "Mobile Nav DropDown ", component: mobile_nav_dropdown},
   {componentName: "Standard Mobile Navbar", component: standard_mobile_navbar},
   {componentName: "Search Navbar", component: searchNav},
+  {componentName: "Standard Logo Navbar", component: standardLogoNav},
 ];
 
 export default navbarCollections;
