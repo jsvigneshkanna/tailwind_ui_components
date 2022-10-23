89ef1205692df14ed1e45021e9903bcbabd291c1
Deepak Suthar <92250394+deepaksuthar40128@users.noreply.github.com>
2022-10-23 15:34:30 +0530

    Create aboutUs.js

---
+++ components/aboutUs.js
@@ -0,0 +1,79 @@
+/* eslint-disable @next/next/no-img-element */
+import { useState, useEffect } from "react";
+import { BallTriangle } from "react-loader-spinner";
+import Link from "next/link";
+
+const AboutUs = () => {
+      const [loading, setloading] = useState(true);
+      useEffect(() => {
+            setTimeout(() => {
+                  setloading(false);
+            }, 600);
+      }, []);
+      fetch('https://api.github.com/repos/jsvigneshkanna/tailwind_ui_components/contributors').then(res => res.json()).then(res => {
+            if (document.getElementById('contr').innerHTML == "") {
+                  for (var xx = 0; xx < res.length; xx++) {
+                        document.getElementById('contributors').innerHTML += ` <a href="${res[xx].url}" class=" overflow-hidden flex flex-col justify-evenly w-28 m-4"><img class="inline rounded-full"
+                                                                  src="${res[xx].avatar_url}"
+                                                                  alt=""/><span
+                                                                  class="text-white"><div class="w-full text-center">${res[xx].login}</div></span></a>`
+                  }
+            }
+      })
+      return (
+            <div>
+                  {loading ? (
+                        <div
+                              style={{
+                                    height: "100vh",
+                                    backgroundColor: "rgba(0,0,0,0.99)",
+                                    display: "flex",
+                                    justifyContent: "center",
+                              }}
+                              className="banner">
+                              <BallTriangle
+                                    height="100"
+                                    width="100"
+                                    color="#e39a09"
+                                    ariaLabel="loading"
+                              />
+                        </div>
+                  ) : (
+                        <div className="banner">
+                              <div>
+                                    <div class="w-full flex justify-evenly flex-wrap">
+                                          <div class="w-5/12 my-5 p-2 m border-white border-2 rounded">
+                                                <p class="w-full text-xl  text-white">Our website motivation</p>
+                                                <hr class="bg-white" />
+                                                <p class="w-full text-white">We do know tailwind css is an emerging CSS framework which makes
+                                                      our website/ app unique without styling compared to
+                                                      other market bootstraps.</p>
+
+                                          </div>
+                                          <div class="w-5/12 my-5 p-2 m border-white border-2 rounded">
+                                                <p class="w-full text-xl text-white">Solution we are providing</p>
+                                                <hr class="bg-white" />
+                                                <p class="w-full text-white">You can get almost all layouts and components built under Tailwind
+                                                      CSS, and the best part is we can play around with them
+                                                      in inbuilt code editor and copy the codebase too for your projects.</p>
+                                          </div>
+                                    </div>
+                                    <div>
+                                          <div>
+                                                <p class=" mt-8 ml-10 text-xl  text-white">Our contributers:</p>
+
+                                                <div class="p-6 px-5 overflow-auto ">
+                                                      <div id="contributors" class="overflow-auto whitespace-nowrap w-auto flex flex-wrap justify-evenly  border-white border-2 rounded">
+
+                                                      </div>
+                                                </div>
+                                          </div>
+                                    </div>
+                              </div>
+                        </div>
+                  )}
+            </div>
+      );
+};
+
+export default AboutUs;
