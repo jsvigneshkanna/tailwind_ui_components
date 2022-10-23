8e13cf5000829e8137fb2623111e9b9f4ffe526b
Shahid <shahid.jamal0343@gmail.com>
2022-10-23 08:50:59 +0500

    feat: file added for icon badge file

--- tailwind_components/badges/collection.js
+++ tailwind_components/badges/collection.js
@@ -1,5 +1,9 @@
 import exampleBadges from "./colorful_badge";
+import singleButton from "./icon_badge";
 
-const badgeCollection = [{componentName: "Example of Badges", component: exampleBadges}];
+const badgeCollection = [
+  {componentName: "Example of Badges", component: exampleBadges},
+  {componentName: "Exampler of Dot Badges", component: singleButton},
+];
 
 export default badgeCollection;
