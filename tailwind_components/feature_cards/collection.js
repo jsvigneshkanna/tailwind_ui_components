8f1ab7600029085f4b7c65350bb79351ccb02dea
Richard Nikolas <richardnikolas1@gmail.com>
2022-10-23 13:27:56 -0300

    [#203] New Presentation Cards

--- tailwind_components/feature_cards/collection.js
+++ tailwind_components/feature_cards/collection.js
@@ -3,13 +3,15 @@ import brandCard from "./brand_card";
 import productCard from "./product_card1";
 import basicCard from "./basic_card";
 import learnmoreCard from "./learn_more_card";
+import presentationCards from "./presentation_card";
 
 const featureCardColletion = [
-  {componentName: "Portfolio card", component: cardWithButton},
-  {componentName: "Brand Card", component: brandCard},
-  {componentName: "Product Card", component: productCard},
-  {componentName: "Basic Card", component: basicCard},
-  {componentName:"Learn More card",component:learnmoreCard},
+  { componentName: "Portfolio card", component: cardWithButton },
+  { componentName: "Brand Card", component: brandCard },
+  { componentName: "Product Card", component: productCard },
+  { componentName: "Basic Card", component: basicCard },
+  { componentName: "Learn More card", component: learnmoreCard },
+  { componentName: "Presentation Cards", component: presentationCards }
 ];
 
 export default featureCardColletion;
