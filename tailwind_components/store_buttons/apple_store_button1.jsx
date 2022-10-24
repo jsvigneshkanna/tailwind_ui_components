6dace59fda986df7481d6fcd3769132681dbe8c0
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-10-24 20:36:27 +0530

    store button plagarism removed

--- tailwind_components/store_buttons/apple_store_button1.jsx
+++ tailwind_components/store_buttons/apple_store_button1.jsx
@@ -1,9 +1,14 @@
-import { stripIndent } from "react-codepen-prefill-embed";
+import {stripIndent} from "react-codepen-prefill-embed";
 
-const appleStoreButton1 = stripIndent`<head>
+const appleStoreButton1 = stripIndent`
+<head>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
 </head>
-<div class="p-5 w-screen bg-slate-400">
+<div class="h-screen bg-sky-300 px-6 pt-10">
+  <h1 class="font-bold text-blue-800">
+    These store buttons are clones and store images are taken from font-awesome
+    (no copyright issue 🙌)
+  </h1>
 <div class="pt-5 px-10">
   <a href="#" class="w-max bg-white rounded-md px-8 py-4 flex gap-5 hover:bg-slate-100">
     <i class="text-5xl text-black fa-brands fa-apple"></i>
@@ -16,6 +21,7 @@ const appleStoreButton1 = stripIndent`<head>
     <span class="text-start text-white font-bold">Download on the<br><span class="font-normal text-xl">App Store</span></p>
   </a>
 </div>
-</div>`;
+</div>
+`;
 
 export default appleStoreButton1;
--- tailwind_components/store_buttons/windows_store_button.jsx
+++ tailwind_components/store_buttons/windows_store_button.jsx
@@ -1,44 +1,54 @@
 import {stripIndent} from "react-codepen-prefill-embed";
 
-  const windowstorebutton = stripIndent`
-  <head>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+const windowstorebutton = stripIndent`
+<head>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+  />
 </head>
 
-  <div class='bg-gray-50'>
-  <div class="mx-auto  max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
-      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-        <span class="block">Ready to dive in?</span>
-        <span class="block text-indigo-600">Start your free trial today.</span>
-      </h2>
-      <div class='mt-8'>
-  <div>
-  <button
-  class='bg-white font-medium text-base border-transparent px-6 py-3 rounded-md flex flex-row items-center gap-4 shadow'>
-  <span>
-  <i class="fa-brands fa-windows text-4xl"></i>
-  </span>
-  <a href='#'>
-  <p class='text-xs font-bold'>Download on the</p>
-  <p class='text-sm'>Windows Store</p>
-  </a>
-</button>
-  </div>
-  <div class='py-4'>
-  <button
-  class='bg-black font-medium text-base border-transparent px-6 py-3 rounded-md flex flex-row items-center gap-4 shadow'>
-  <span>
-  <i class="fa-brands text-white fa-windows text-4xl"></i>
-  </span>
-  <a class='text-white block' href='#'>
-  <p class='text-xs font-bold'>Download on the</p>
-  <p class='text-sm'>Windows Store</p>
-  </a>
-</button>
+<div class="h-screen bg-sky-300 px-6 pt-10">
+  <h1 class="font-bold text-blue-800">
+    These store buttons are clones and store images are taken from font-awesome
+    (no copyright issue 🙌)
+  </h1>
+  <div
+    class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8"
+  >
+    <div class="mt-8">
+      <div>
+        <button
+          class="flex flex-row items-center gap-4 rounded-md border-transparent bg-white px-6 py-3 text-base font-medium shadow"
+        >
+          <span>
+            <i class="fa-brands fa-windows text-4xl"></i>
+          </span>
+          <a href="https://tailwindcsscomponents.vercel.app/">
+            <p class="text-xs font-bold">Download on the</p>
+            <p class="text-sm">Windows Store</p>
+          </a>
+        </button>
+      </div>
+      <div class="py-4">
+        <button
+          class="flex flex-row items-center gap-4 rounded-md border-transparent bg-black px-6 py-3 text-base font-medium shadow"
+        >
+          <span>
+            <i class="fa-brands fa-windows text-4xl text-white"></i>
+          </span>
+          <a
+            class="block text-white"
+            href="https://tailwindcsscomponents.vercel.app/"
+          >
+            <p class="text-xs font-bold">Download on the</p>
+            <p class="text-sm">Windows Store</p>
+          </a>
+        </button>
+      </div>
+    </div>
   </div>
 </div>
-</div>
-</div>    
-  `
+`;
 
-  export default windowstorebutton
+export default windowstorebutton;
