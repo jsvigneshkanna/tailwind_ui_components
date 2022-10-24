df3417397ded56056cfac05057e4403b57095d5b
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-10-24 21:10:45 +0530

    info alert padding reduced

--- tailwind_components/alerts/info_alert.jsx
+++ tailwind_components/alerts/info_alert.jsx
@@ -1,7 +1,7 @@
 import {stripIndent} from "react-codepen-prefill-embed";
 
 const infoAlert = stripIndent`
-<div class="h-screen bg-sky-300 px-20 pt-4">
+<div class="h-screen bg-sky-300 px-4 pt-4">
   <div class="relative m-4 rounded border-0 bg-green-500 px-6 py-4 text-white">
     <span class="mr-5 inline-block align-middle text-xl">
       <i class="fas fa-bell"></i>
