e4cc1d8999c5fcc480fe61ed25d74ebf4f74d1bc
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-10-24 23:00:16 +0530

    badges plagarism removed

--- tailwind_components/badges/card_badge.jsx
+++ tailwind_components/badges/card_badge.jsx
@@ -1,15 +1,26 @@
 import {stripIndent} from "react-codepen-prefill-embed";
 
 const CardBadge = stripIndent`
-<div class="relative mx-10 my-10 max-w-sm rounded overflow-hidden shadow-lg"> 
- <span class="absolute right-0 top-0 inline-block py-1.5 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-red-600 text-xl text-white rounded">New</span>
+<div class="h-full bg-rose-300 pt-10">
+  <div
+    class="relative mx-10 max-w-sm rounded-3xl bg-sky-200 shadow-lg shadow-blue-500"
+  >
+    <span
+      class="absolute right-[-20px] top-[-20px] inline-block whitespace-nowrap rounded-full bg-red-600 py-4 px-4 text-center align-baseline text-xl font-bold leading-none text-white shadow-2xl shadow-teal-400"
+      >New</span
+    >
 
-<img class="w-full" src="https://media.istockphoto.com/photos/hot-air-balloons-picture-id184091124?s=612x612" alt="VK tailwind Bootstrap">
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">Welcome to VK tailwind Bootstrap</div>
-    <p class="text-gray-700 text-base">
-      Best part of card badge is to have a badge above a card. Nothing else 😆
-    </p>
+    <img
+      class="w-full"
+      src="https://media.istockphoto.com/photos/hot-air-balloons-picture-id184091124?s=612x612"
+      alt="VK tailwind Bootstrap"
+    />
+    <div class="px-6 py-4">
+      <div class="mb-2 text-xl font-bold">Welcome to VK tailwind Bootstrap</div>
+      <p class="pb-6 text-lg font-medium text-blue-900">
+        Best part of card badge is to have a badge above a card. Nothing else 😆
+      </p>
+    </div>
   </div>
 </div>
 `;
