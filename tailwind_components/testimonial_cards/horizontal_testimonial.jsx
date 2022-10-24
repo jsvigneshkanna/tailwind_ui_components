02f25113e50334718c841a259f25ba9da916876f
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-10-24 22:23:47 +0530

    testimonial card plagarism removed

--- tailwind_components/testimonial_cards/horizontal_testimonial.jsx
+++ tailwind_components/testimonial_cards/horizontal_testimonial.jsx
@@ -1,57 +1,60 @@
 import {stripIndent} from "react-codepen-prefill-embed";
 
 const horizontalTestimonial = stripIndent`
-<div class="flex h-screen w-screen items-center justify-center bg-slate-400">
-  <div class="h-64 w-96 max-w-sm rounded-xl border border-black bg-white shadow-md">
-    <a href="#"> </a>
+<div class="flex h-screen w-screen items-center justify-center bg-sky-300">
+  <div class="h-64 w-96 max-w-sm rounded-xl border border-black bg-white shadow-lg shadow-red-400">
     <div class="p-5 text-center">
-      <a href="#"> </a>
-  
-  <ul class = "flex">
-      <div class="bg-blend-multiply">
-        <div class="h-0">
-          <nav class = "mx-4 -ml-5 -mt-5 mb-0 ">
-          <a href="https://github.com/jsvigneshkanna/tailwind_ui_components"><img src=https://images.unsplash.com/photo-1522003878398-14c13adf3c5d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9" alt="" height="170" width="140" class = "object-cover h-60 w-60 rounded-t-md"  > </a>
-          </nav>
+      <ul class="flex">
+        <div class="bg-blend-multiply">
+          <div class="h-0">
+            <nav class="mx-4 -ml-5 -mt-5 mb-0"><img src=https://images.unsplash.com/photo-1522003878398-14c13adf3c5d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9" alt="" height="170" width="140" class = "object-cover h-60 w-60 rounded-t-md" ></nav>
+          </div>
         </div>
-      </div>
 
-  <ul class = "flex-col">
+        <ul class="flex-col">
+          <div class="text-bl -tracking-tight- mx-2 text-left font-normal text-gray-700">
+            <a href="https://github.com/jsvigneshkanna" target="_blank" rel="noreferrer">
+              <h3 class="m space-y-6 text-blue-700"><b>J S Vignesh Kanna</b></h3>
+              <ul class="my-1">
+                Software Engineer
 
-    <div class=" text-bl mx-2 text-left font-normal -tracking-tight- text-gray-700">
-          <a href="https://youtu.be/dQw4w9WgXcQ">
-            <h3 class="text-blue-700 space-y-6 m"><b>Marzia Schills</b></h3>
-            <ul class = "my-1">Designer
-
-    <ul class="">
-        <div class="mb-0 my-3 -mx-1 flex items-end">
-          <svg aria-hidden="true" class="h-7 w-7 text-orange-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <title>First star</title>
-            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-          </svg>
-          <svg aria-hidden="true" class="h-7 w-7 text-orange-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <title>Second star</title>
-            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-          </svg>
-          <svg aria-hidden="true" class="h-7 w-7 text-orange-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <title>Third star</title>
-            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-          </svg>
-          <svg aria-hidden="true" class="h-7 w-7 text-orange-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <title>Fourth star</title>
-            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-          </svg>
-          <svg aria-hidden="true" class="h-7 w-7 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <title>Fifth star</title>
-            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-          </svg>
-        </div>
+                <ul class="">
+                  <div class="my-3 -mx-1 mb-0 flex items-end">
+                    <svg aria-hidden="true" class="h-7 w-7 text-orange-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                      <title>First star</title>
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                    </svg>
+                    <svg aria-hidden="true" class="h-7 w-7 text-orange-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                      <title>Second star</title>
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                    </svg>
+                    <svg aria-hidden="true" class="h-7 w-7 text-orange-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                      <title>Third star</title>
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                    </svg>
+                    <svg aria-hidden="true" class="h-7 w-7 text-orange-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                      <title>Fourth star</title>
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                    </svg>
+                    <svg aria-hidden="true" class="h-7 w-7 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                      <title>Fifth star</title>
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                    </svg>
+                  </div>
+                </ul>
+                <ul class="flex-col space-y-0">
+                  <p class="mb-3 font-normal text-gray-600"></p>
+                  <p class="mb-3 font-normal text-gray-600">This proved to be impossible using concepts of space and</p>
+                  <p class="mb-3 font-normal text-gray-600">time. Einstein new view of</p>
+                  <p class="mb-3 space-y-8 font-normal text-gray-600">time first and then</p>
+                </ul>
+              </ul></a
+            >
+          </div>
+        </ul>
       </ul>
-    <ul class = "flex-col space-y-0">
-      <p class="mb-3 font-normal text-gray-600">
-       <p class="mb-3 font-normal text-gray-600">This proved to be impossible using concepts of space and  </p> 
-       <p class="mb-3 font-normal text-gray-600">time. Einstein new view of </p>
-      <p class=" space-y-8 mb-3 font-normal text-gray-600"> time first and then</p>
-    </ul>
+    </div>
+  </div>
+</div>
 `;
 export default horizontalTestimonial;
