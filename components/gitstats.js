ae1c83aa56e5335f9a1ca74efa5f43ceb3623e57
AmanJain18 <89217032+AmanJain18@users.noreply.github.com>
2022-10-28 03:35:43 +0530

    fixed bug issue #249

--- components/gitstats.js
+++ components/gitstats.js
@@ -2,7 +2,7 @@
 import React from "react";
 import {TbGitFork} from "react-icons/tb";
 import {BsStarFill} from "react-icons/bs";
-
+import cn from 'classnames';
 import useSWR from "swr";
 
 async function fetcher(...arg) {
@@ -10,12 +10,13 @@ async function fetcher(...arg) {
 
   return res.json();
 }
-const GitStats = () => {
+const GitStats = (props) => {
+  const {className} = props;
   const {data} = useSWR("/api/github", fetcher);
 
   return (
     <a
-      className="gitstats_box"
+      className={cn("gitstats_box", className)}
       href="https://github.com/jsvigneshkanna/tailwind_ui_components"
       target="__blank">
       <div className="gitstats_fork">
