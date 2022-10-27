22a84a474d4844724f88b6e672dc2004fab34585
J S Vignesh Kanna <42484705+jsvigneshkanna@users.noreply.github.com>
2022-10-27 18:49:31 +0530

    Update collection.js

--- tailwind_components/feature_cards/collection.js
+++ tailwind_components/feature_cards/collection.js
@@ -1,27 +1,19 @@
 import cardWithButton from "./card_with_button_button1";
 import brandCard from "./brand_card";
 import productCard from "./product_card1";
-<<<<<<< HEAD
 import contactCard from "./contact_card.jsx"
+import basicCard from "./basic_card";
+import learnmoreCard from "./learn_more_card";
+import presentationCards from "./presentation_card";
 
 const featureCardColletion = [
   {componentName: "Card With Button", component: cardWithButton},
   {componentName: "Brand Card", component: brandCard},
   {componentName: "Product Card", component: productCard},
   {componentName: "Contact Card", component: contactCard},
-=======
-import basicCard from "./basic_card";
-import learnmoreCard from "./learn_more_card";
-import presentationCards from "./presentation_card";
-
-const featureCardColletion = [
-  { componentName: "Portfolio card", component: cardWithButton },
-  { componentName: "Brand Card", component: brandCard },
-  { componentName: "Product Card", component: productCard },
   { componentName: "Basic Card", component: basicCard },
   { componentName: "Learn More card", component: learnmoreCard },
   { componentName: "Presentation Cards", component: presentationCards }
->>>>>>> 0039c2d5d909128c6c629f252150ede2a881305f
 ];
 
 export default featureCardColletion;
