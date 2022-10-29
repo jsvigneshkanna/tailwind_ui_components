6a9e0c232ceca44e865d35b5759a5a1958609667
AmanJain18 <89217032+AmanJain18@users.noreply.github.com>
2022-10-29 21:48:30 +0530

    Social Fix

--- components/socialbuttons.js
+++ components/socialbuttons.js
@@ -4,27 +4,16 @@ const SocialButtons = () => {
   return (
     <div className="flex flex-row w-100 items-center justify-center md:flex-row bg-gradient-to-tr from-[#000855] to-[#690101] py-3 md:py-6">
       <div className=" hidden md9:flex flex-row items-center justify-center">
-<<<<<<< HEAD
-        <div className="group my-3 mx-5 inline-block h-16 w-16 py-0 hover:w-52 transition-all ease-out bg-white rounded-full float-left overflow-hidden cursor-pointer">
-          <div className="inline-block h-16 w-16 text-center bg-zinc-900 transition-all ease-out box-border rounded-full leading-[65px] group-hover:bg-[#4267B2]">
-            <i className="fab fa-facebook-f text-2xl leading-[60px] transition-all ease-out text-white"></i>
-=======
         <div className="group m-3 inline-block h-16 w-16 py-0 hover:w-52 transition-all ease-out  rounded-[10px] float-left overflow-hidden cursor-pointer">
           <div className="inline-block h-16 w-16 text-center bg-[#292825fa] transition-all  ease-in-out duration-300 box-border rounded-[10px] leading-[65px] group-hover:bg-[#4267B2]">
             <i className="fab fa-facebook-f text-2xl leading-[60px] transition-all ease-in-out duration-300 text-white"></i>
->>>>>>> ba0a9b6397d6ffc883a3fcd28cc695b8026a53f5
           </div>
           <span className="text-xl bg-white p-[15px] rounded-[10px] font-medium ml-2 leading-[65px] transition-all ease-out group-hover:text-[#4267B2]">
             <Link href="#">Facebook</Link>
           </span>
         </div>
-<<<<<<< HEAD
-        <div className="group my-3 mx-5 inline-block h-16 w-16 py-0 hover:w-52 transition-all ease-out bg-white rounded-full float-left overflow-hidden cursor-pointer">
-          <div className="inline-block h-16 w-16 text-center bg-zinc-900 transition-all ease-out box-border rounded-full leading-[65px] group-hover:bg-[#1DA1F2]">
-=======
         <div className="group m-3 inline-block h-16 w-16 py-0  hover:w-52 transition-all ease-in-out duration-300  rounded-[10px]  float-left overflow-hidden cursor-pointer">
           <div className="inline-block h-16 w-16 text-center bg-[#292825fa]  transition-all ease-in-out duration-300 box-border rounded-[10px]  leading-[65px] group-hover:bg-[#1DA1F2]">
->>>>>>> ba0a9b6397d6ffc883a3fcd28cc695b8026a53f5
             <i className="fab fa-twitter text-2xl leading-[60px] transition-all ease-out text-white"></i>
           </div>
           <span className="text-xl bg-white p-[15px] rounded-[10px]  font-medium ml-2 leading-[65px] transition-all ease-out group-hover:text-[#1DA1F2]">
@@ -33,13 +22,8 @@ const SocialButtons = () => {
         </div>
       </div>
       <div className="flex flex-row items-center justify-center">
-<<<<<<< HEAD
-        <div className=" hidden md9:block group my-3 mx-5 h-16 w-16 py-0  hover:w-52 transition-all ease-out bg-white rounded-full float-left overflow-hidden cursor-pointer">
-          <div className="inline-block h-16 w-16 text-center bg-zinc-900 transition-all ease-out box-border rounded-full leading-[65px] group-hover:bg-[#E1306C]">
-=======
         <div className="hidden md9:block group m-3 h-16 w-16 py-0  hover:w-52 transition-all ease-in-out duration-300  rounded-[10px]  float-left overflow-hidden cursor-pointer">
           <div className="inline-block h-16 w-16 text-center bg-[#292825fa]  transition-all ease-in-out duration-300 box-border rounded-[10px] leading-[65px] group-hover:bg-[#E1306C]">
->>>>>>> ba0a9b6397d6ffc883a3fcd28cc695b8026a53f5
             <i className="fab fa-instagram text-2xl leading-[60px] transition-all ease-out text-white"></i>
           </div>
           <span className="text-xl bg-white p-[15px] rounded-[10px]  font-medium ml-2 leading-[65px] transition-all ease-out group-hover:text-[#E1306C]">
@@ -47,13 +31,8 @@ const SocialButtons = () => {
           </span>
         </div>
 
-<<<<<<< HEAD
-        <div className="group my-3 mx-5 inline-block h-16 w-16 py-0 hover:w-52 transition-all ease-out bg-white rounded-full float-left overflow-hidden cursor-pointer">
-          <div className="inline-block h-16 w-16 text-center bg-zinc-900 transition-all ease-out box-border rounded-full leading-[65px] group-hover:bg-[#333]">
-=======
         <div className="group m-3 inline-block h-16 w-16 py-0 hover:w-52 transition-all ease-in-out duration-300  rounded-[10px]  float-left overflow-hidden cursor-pointer">
           <div className="inline-block h-16 w-16 text-center bg-[#292825fa] transition-all ease-in-out duration-300 box-border rounded-[10px]  leading-[65px] group-hover:bg-[#333]">
->>>>>>> ba0a9b6397d6ffc883a3fcd28cc695b8026a53f5
             <i className="fab fa-github text-2xl leading-[60px] transition-all ease-out text-white"></i>
           </div>
           <span className="text-xl bg-white p-[15px] rounded-[10px]  font-medium ml-2 leading-[65px] transition-all ease-out group-hover:text-[#333]">
@@ -67,26 +46,16 @@ const SocialButtons = () => {
         </div>
       </div>
       <div className="flex flex-row items-center justify-center">
-<<<<<<< HEAD
-        <div className=" hidden md9:block group my-3 mx-5 h-16 w-16 py-0 hover:w-52 transition-all ease-out bg-white rounded-full float-left overflow-hidden cursor-pointer">
-          <div className="inline-block h-16 w-16 text-center bg-zinc-900 transition-all ease-out box-border rounded-full leading-[65px] group-hover:bg-[#ff0000]">
-=======
         <div className="  hidden md9:block group m-3 h-16 w-16 py-0 hover:w-52 transition-all ease-in-out duration-300 rounded-[10px]  float-left overflow-hidden cursor-pointer">
           <div className="inline-block h-16 w-16 text-center bg-[#292825fa]  transition-all ease-in-out duration-300 box-border rounded-[10px] leading-[65px] group-hover:bg-[#ff0000]">
->>>>>>> ba0a9b6397d6ffc883a3fcd28cc695b8026a53f5
             <i className="fab fa-youtube text-2xl leading-[60px] transition-all ease-out text-white"></i>
           </div>
           <span className="text-xl  bg-white p-[15px] rounded-[10px]  font-medium ml-2 leading-[65px] transition-all ease-out group-hover:text-[#ff0000]">
             <Link href="#">YouTube</Link>
           </span>
         </div>
-<<<<<<< HEAD
-        <div className="group my-3 mx-5 inline-block h-16 w-16 py-0 hover:w-52 transition-all ease-out bg-white rounded-full float-left overflow-hidden cursor-pointer">
-          <div className="inline-block h-16 w-16 text-center bg-zinc-900 transition-all ease-out box-border rounded-full leading-[65px] group-hover:bg-[#0A66C2]">
-=======
         <div className="group m-3 inline-block h-16 w-16 py-0 hover:w-52 transition-all ease-in-out duration-300 rounded-[10px]  float-left overflow-hidden cursor-pointer">
           <div className="inline-block h-16 w-16 text-center bg-[#292825fa]  transition-all ease-in-out duration-300 box-border rounded-[10px]  leading-[65px] group-hover:bg-[#0A66C2]">
->>>>>>> ba0a9b6397d6ffc883a3fcd28cc695b8026a53f5
             <i className="fab fa-linkedin text-2xl leading-[60px] transition-all ease-out text-white"></i>
           </div>
           <span className="text-xl bg-white p-[15px] rounded-[10px]  font-medium ml-2 leading-[65px] transition-all ease-out group-hover:text-[#0A66C2]">
