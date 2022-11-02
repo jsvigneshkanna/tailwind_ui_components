fc50216dd7a51bcceac59cb37aea3c749c89ab02
AmanJain18 <89217032+AmanJain18@users.noreply.github.com>
2022-11-02 16:19:50 +0530

    Style and Fixed Faqs Sections

--- components/faqs.js
+++ components/faqs.js
@@ -1,98 +1,102 @@
 /* eslint-disable @next/next/no-img-element */
 import {useEffect} from "react";
+import $ from "jquery";
 import Link from "next/link";
+import mitt from "next/dist/shared/lib/mitt";
 
 const FAQs = () => {
   useEffect(() => {
     setTimeout(() => {
       const data = {
         q1: {
-          qus: "What is Tailwind Bootstrap",
-          ans: "Tailwind css is an emerging CSS framework which makes our website/ app unique without styling compared to other market bootstraps.",
+          qes: "What is Tailwind Bootstrap ?",
+          ans: "Tailwind css is an emerging CSS framework which makes our website / app unique without styling compared to other market bootstraps.",
         },
         q2: {
-          qus: "Who drives this website",
-          ans: 'Our Contributers Drive the website (around 50+ open source contributors - and growing in 🚀 pace), for more visit <a href="/aboutUs" target="_blank" rel="noreferrer" class="text-blue-700">Here</a>',
+          qes: "Who drives this website ?",
+          ans: 'Our Contributers Drive the website (around 50+ open source contributors - and growing in 🚀 pace), for more visit <a href="/aboutUs" target="_blank" rel="noreferrer" class="text-fuchsia-500 underline">Here.</a>',
         },
         q3: {
-          qus: "What all components are there in this website",
-          ans: 'There are lots of components such as Button, Form, Feature Card etc. which can save lot of time for a front end dev. you can access them from <a href="/components" target="_blank" rel="noreferrer" class="text-blue-700">Here</a>',
+          qes: "What all components are there in this website ?",
+          ans: 'There are lots of components such as Button, Form, Feature Card etc. Which can save lot of time for a Frontv End Dev. You can access them from <a href="/components" target="_blank" rel="noreferrer" class="text-fuchsia-500 underline">Here.</a>',
         },
         q4: {
-          qus: "Can we play around with tailwindcss codes",
-          ans: "Yes,You can. We actually have embeded codepen in each component section. You can modify the code/ styles and rerun the codepen to get required design ⚡",
+          qes: "Can we play around with tailwindcss codes ?",
+          ans: "Yes, You can. We actually have embeded codepen in each component section. You can modify the code / styles and rerun the codepen to get required design. ⚡",
         },
         q5: {
-          qus: "Is there any pricing or subscription",
-          ans: 'Nooooo 🙅,It is free of cost. We will atmost thrive to keep this website open sourced and free of cost to users. If you like our product very much and wish to contribute us some money which we use to enhance further, you can reach us at <a href="https://www.buymeacoffee.com/jsvigneshkanna" target="_blank" rel="noreferrer" class="text-blue-700">Here</a>',
+          qes: "Is there any pricing or subscription ?",
+          ans: 'Nooooo 🙅, It is free of cost. We will atmost thrive to keep this website open sourced and free of cost to users. If you like our product very much and wish to contribute us some money which we use to enhance further, you can reach us at <a href="https://www.buymeacoffee.com/jsvigneshkanna" target="_blank" rel="noreferrer" class="text-fuchsia-500 underline">Here.</a>',
         },
         q6: {
-          qus: "How can we use these UI components and where can we find documents",
-          ans: 'You can actually play around with existing component codes in each component section, copy them and use them in your codebase. For more information check <a href="/documentation" target="_blank" rel="noreferrer" class="text-blue-700">Docs</a>',
+          qes: "How can we use these UI components and where can we find documents ?",
+          ans: 'You can actually play around with existing component codes in each component section, copy them and use them in your codebase. For more information check <a href="/documentation" target="_blank" rel="noreferrer" class="text-fuchsia-500 underline">Docs.</a>',
         },
         q7: {
-          qus: "I would like to contribute to this website, how can I proceed?",
-          ans: 'Hurray 🕺💃, You can contribute from <a href="https://github.com/jsvigneshkanna/tailwind_ui_components" class="text-blue-700">Here</a>. We are so happy, that you checked our product and like to contribute !',
+          qes: "I would like to contribute to this website, how can I proceed ?",
+          ans: 'Hurray 🕺💃, You can contribute from <a href="https://github.com/jsvigneshkanna/tailwind_ui_components" class="text-fuchsia-500 underline">Here.</a> We are so happy, that you checked our product and like to contribute !!!',
         },
       };
       if (document.getElementById("faqs").innerHTML == "") {
         for (const q in data) {
-          document.getElementById(
-            "faqs",
-          ).innerHTML += `<div class="border-b font-medium border-black my-2 overflow-hidden">
-                                    <div class="flex justify-between p-3 items-center ">
-                                          <p>${data[q].qus}</p>
-                                          <p class="ml-2 border border-black rounded-full  answer_btn"><svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                                </svg></p>
-                                                </div>
-                                                <hr>
-                                                <p class="hidden p-3">${data[q].ans}</p>
-                                                </div>`;
+          document.getElementById("faqs").innerHTML += `
+          <div class="wrapper bg-white mb-5 py-4 px-8 rounded-md shadow-wrap">
+            <button class="toggle w-full text-xl bg-transparent flex justify-between items-center font-semibold border-none outline-none cursor-pointer py-4 px-0 focus:outline-none text-amber-500 ">
+                ${data[q].qes}
+                <i class="fas fa-plus icon p-2"></i>
+            </button>
+            <div class="content relative md:text-xl font-medium text-blue-800 text-justify max-h-0 leading-7 md:leading-8 overflow-hidden transition-mh ease-in-out duration-300 before:content-[''] before:absolute before:h-90 before:w-2 before:top-1/2 before:left-0 before:-translate-y-1/2 before:bg-faq1 bg-sky-50">
+                <p class="p-4">${data[q].ans}</p>
+            </div>
+        </div>
+                                    `;
         }
       }
-      Array.from(document.getElementsByClassName("answer_btn")).forEach((e) => {
-        e.addEventListener("click", (ee) => {
-          let ele = e.parentElement.parentElement.children[2];
-          let ele1 = e.parentElement.children[0];
-          let flag = false;
-          Array.from(ele.classList).forEach((classes) => {
-            if (classes == "hidden") flag = true;
-          });
-          if (flag) {
-            ele.classList.remove("hidden");
-            e.classList.add("origin-center");
-            e.classList.add("rotate-180");
-            e.classList.add("bg-blue-500");
-            e.classList.remove("border");
-            e.classList.add("text-white");
-            ele1.classList.add("text-blue-500");
+      let toggles = document.getElementsByClassName("toggle");
+      let contentDiv = document.getElementsByClassName("content");
+      let icons = document.getElementsByClassName("icon");
+
+      for (let i = 0; i < toggles.length; i++) {
+        toggles[i].addEventListener("click", () => {
+          if (
+            parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight
+          ) {
+            toggles[i].style.color = "rgb(217 70 239)";
+            icons[i].classList.remove("fa-plus");
+            icons[i].classList.add("fa-minus");
+            contentDiv[i].classList.add("max-h-80");
+            contentDiv[i].classList.remove("max-h-0");
           } else {
-            ele.classList.add("hidden");
-            e.classList.remove("origin-center");
-            e.classList.remove("rotate-180");
-            e.classList.remove("bg-blue-500");
-            e.classList.add("border");
-            e.classList.remove("text-white");
-            ele1.classList.remove("text-blue-500");
+            contentDiv[i].classList.remove("max-h-80");
+            toggles[i].style.color = "rgb(245 158 11)";
+            icons[i].classList.remove("fa-minus");
+            icons[i].classList.add("fa-plus");
+            contentDiv[i].classList.add("max-h-0");
+          }
+          for (let j = 0; j < contentDiv.length; j++) {
+            if (j !== i) {
+              contentDiv[j].classList.add("max-h-0");
+              toggles[j].style.color = "rgb(245 158 11)";
+              icons[j].classList.remove("fa-minus");
+              icons[j].classList.add("fa-plus");
+              contentDiv[j].classList.remove("max-h-80");
+            }
           }
         });
-      });
-    }, 100);
+      }
+    }, 10);
   }, []);
 
   return (
     <div>
       <div className="banner bannerfaq">
         <div class="pb-6 text-white">
-          <h1 class="p-3 text-center font-semibold text-lg md:text-2xl underline underline-offset-8">
-            Frequently Asked Questions
+          <h1 class="px-3 mt-8 text-center font-semibold text-lg md:text-2xl underline underline-offset-8 font-sans">
+            Frequently Asked Questions ?
           </h1>
           <div
             id="faqs"
-            class="text-black bg-stone-300 border m-auto mt-9 mx-4 md:mx-40 px-3 md:px-8 py-12 rounded-2xl border-black"></div>
+            class="text-black m-auto mx-6 mt-9 md9:mx-20 xl:!mx-36 lg:!mx-32 px-3 md:px-8 py-10 max-w-96 font-sans"></div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,33 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    minWidth: {
+      500: "500px",
+    },
+    boxShadow: {
+      wrap: "0 15px 25px  rgba(0, 0, 50, 0.3)",
+    },
     extend: {
+      transitionProperty: {
+        mh: "max-height",
+      },
+      transitionDuration: {
+        1000: "1s",
+        650: "650ms",
+      },
+      width: {
+        45: "45%",
+        0.6: "0.6rem",
+      },
+      height: {
+        90: "90%",
+      },
       screens: {
         md9: "900px",
-        // => @media (min-width: 900px) { ... }
       },
       colors: {
         "alert-text": "#d4975c",
+        faq1: "#8ffcff",
       },
       fontFamily: {
         poppins: ["poppins"],
